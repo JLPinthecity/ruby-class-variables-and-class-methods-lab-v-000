6c9b6a4672612151cf611abf568f6b3a634b8987
Joann Pan <joannlpan@gmail.com>
2020-05-08 20:44:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,26 @@
 class Song
   attr_accessor :name, :artist, :genre
 
+  @@count = 0
+  @@genres = []
+
   def initialize
+    @@count += 0
+    @@genres << self.genre if @@genres !include?(self.name)
   end
 
   def count
+    @@count
   end
 
+  def artists
+  end
+
+  def genres
+    
+  end
+
+  def genre_count
+  end
 
 end
